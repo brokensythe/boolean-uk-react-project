7349f3d5c01eed4364321baf6f13826f68bd09b4
brokensythe <ade_1.0@live.co.uk>
2021-07-05 13:46:07 +0100

    Login page styled

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import LoginPage from "./Components/LoginPage";
 
 export default function App() {
-  return <Switch>
-    <Route path="/">
-      <LoginPage />
-    </Route>
-  </Switch>
+  return  <>
+          <Switch>
+            <Route path="/">
+              <LoginPage />
+            </Route>
+          </Switch>
+          <footer className="footer">
+          &#169; Ade Inc. circa 2021
+          </footer>
+          </>
 }
--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -3,21 +3,18 @@ import LoginForm from "./LoginForm"
 
 function LoginPage() {
     return <>
-    <header className="header">
-        <img
-        src="https://boolean.co.uk/favicon-32x32.png"
-        alt="logo"
-        width="50px"
-      />
-      <h1>Playlist Editor</h1>
-    </header>
-    <main className="main-body">
-       <LoginForm />
-    </main>
-    <footer>
-        &#169; Ade Inc. circa 2021
-    </footer>
-    </>
+            <header className="header">
+                <img
+                src="https://boolean.co.uk/favicon-32x32.png"
+                alt="logo"
+                width="50px"
+            />
+            <h1>Playlist Editor</h1>
+            </header>
+            <main className="main-body">
+                <LoginForm />
+            </main>
+            </>
 }
 
 export default LoginPage
